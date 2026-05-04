9f53369c52e95fbe3c4cd736db7b5fc2ae95001a
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-05-04 19:32:32 +0700

    masukan static mahasiswa

--- constuctorpverloading.cpp
+++ constuctorpverloading.cpp
@@ -15,3 +15,30 @@ class mahasiswa
     void cetak();
 };
 
+mahasiswa::mahasiswa()
+{
+   
+}
+
+mahasiswa::mahasiswa(int iNim)
+{
+    nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama)
+{
+    nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama)
+{
+    nim = iNim;
+    nama = iNama;
+}
+
+void mahasiswa::cetak() 
+{
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
