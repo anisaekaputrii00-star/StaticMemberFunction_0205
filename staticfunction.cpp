1b3af858b97c35b75adbbfcd099b11ef6f2d0a15
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-05-04 19:18:45 +0700

    masukan nama mahasiswa

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -39,3 +39,15 @@ void mahasiswa::printAll(){
     cout << endl;
 }
 
+int main() {
+    mahasiswa mhs1("Sri Dadi");
+    mahasiswa mhs2("Budi Jatmiko");
+    
+    mahasiswa::setNim(9);
+    mahasiswa mhs3("Andi Janu");
+    mahasiswa mhs4("Joko Wohono");
+
+    
+
+}
+
